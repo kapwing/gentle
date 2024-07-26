4f91f3bc36e9c9002cffaa679e7296a5c4d858ca
James McGivern <james@kapwing.com>
2024-07-26 14:08:02 -0700

    format serve.py

--- serve.py
+++ serve.py
@@ -17,6 +17,7 @@ from gentle.util.cyst import Insist
 
 import gentle
 
+
 class TranscriptionStatus(Resource):
     def __init__(self, status_dict):
         self.status_dict = status_dict
@@ -26,21 +27,24 @@ class TranscriptionStatus(Resource):
         req.setHeader(b"Content-Type", "application/json")
         return json.dumps(self.status_dict).encode()
 
-class Transcriber():
+
+class Transcriber:
     def __init__(self, data_dir, nthreads=4, ntranscriptionthreads=2):
         self.data_dir = data_dir
         self.nthreads = nthreads
         self.ntranscriptionthreads = ntranscriptionthreads
         self.resources = gentle.Resources()
 
-        self.full_transcriber = gentle.FullTranscriber(self.resources, nthreads=ntranscriptionthreads)
+        self.full_transcriber = gentle.FullTranscriber(
+            self.resources, nthreads=ntranscriptionthreads
+        )
         self._status_dicts = {}
 
     def get_status(self, uid):
         return self._status_dicts.setdefault(uid, {})
 
     def out_dir(self, uid):
-        return os.path.join(self.data_dir, 'transcriptions', uid)
+        return os.path.join(self.data_dir, "transcriptions", uid)
 
     # TODO(maxhawkins): refactor so this is returned by transcribe()
     def next_id(self):
@@ -53,73 +57,80 @@ class Transcriber():
 
         status = self.get_status(uid)
 
-        status['status'] = 'STARTED'
-        output = {
-            'transcript': transcript
-        }
+        status["status"] = "STARTED"
+        output = {"transcript": transcript}
 
-        outdir = os.path.join(self.data_dir, 'transcriptions', uid)
+        outdir = os.path.join(self.data_dir, "transcriptions", uid)
 
-        tran_path = os.path.join(outdir, 'transcript.txt')
-        with open(tran_path, 'w') as tranfile:
+        tran_path = os.path.join(outdir, "transcript.txt")
+        with open(tran_path, "w") as tranfile:
             tranfile.write(transcript)
-        audio_path = os.path.join(outdir, 'upload')
-        with open(audio_path, 'wb') as wavfile:
+        audio_path = os.path.join(outdir, "upload")
+        with open(audio_path, "wb") as wavfile:
             wavfile.write(audio)
 
-        status['status'] = 'ENCODING'
+        status["status"] = "ENCODING"
 
-        wavfile = os.path.join(outdir, 'a.wav')
-        if gentle.resample(os.path.join(outdir, 'upload'), wavfile) != 0:
-            status['status'] = 'ERROR'
-            status['error'] = "Encoding failed. Make sure that you've uploaded a valid media file."
+        wavfile = os.path.join(outdir, "a.wav")
+        if gentle.resample(os.path.join(outdir, "upload"), wavfile) != 0:
+            status["status"] = "ERROR"
+            status["error"] = (
+                "Encoding failed. Make sure that you've uploaded a valid media file."
+            )
             # Save the status so that errors are recovered on restart of the server
             # XXX: This won't work, because the endpoint will override this file
-            with open(os.path.join(outdir, 'status.json'), 'w') as jsfile:
+            with open(os.path.join(outdir, "status.json"), "w") as jsfile:
                 json.dump(status, jsfile, indent=2)
             return
 
-        #XXX: Maybe we should pass this wave object instead of the
+        # XXX: Maybe we should pass this wave object instead of the
         # file path to align_progress
-        wav_obj = wave.open(wavfile, 'rb')
-        status['duration'] = wav_obj.getnframes() / float(wav_obj.getframerate())
-        status['status'] = 'TRANSCRIBING'
+        wav_obj = wave.open(wavfile, "rb")
+        status["duration"] = wav_obj.getnframes() / float(wav_obj.getframerate())
+        status["status"] = "TRANSCRIBING"
 
         def on_progress(p):
             print(p)
-            for k,v in p.items():
+            for k, v in p.items():
                 status[k] = v
 
         if len(transcript.strip()) > 0:
-            trans = gentle.ForcedAligner(self.resources, transcript, nthreads=self.nthreads, **kwargs)
+            trans = gentle.ForcedAligner(
+                self.resources, transcript, nthreads=self.nthreads, **kwargs
+            )
         elif self.full_transcriber.available:
             trans = self.full_transcriber
         else:
-            status['status'] = 'ERROR'
-            status['error']  = 'No transcript provided and no language model for full transcription'
+            status["status"] = "ERROR"
+            status["error"] = (
+                "No transcript provided and no language model for full transcription"
+            )
             return
 
         output = trans.transcribe(wavfile, progress_cb=on_progress, logging=logging)
 
         # ...remove the original upload
-        os.unlink(os.path.join(outdir, 'upload'))
+        os.unlink(os.path.join(outdir, "upload"))
 
         # Save
-        with open(os.path.join(outdir, 'align.json'), 'w') as jsfile:
+        with open(os.path.join(outdir, "align.json"), "w") as jsfile:
             jsfile.write(output.to_json(indent=2))
-        with open(os.path.join(outdir, 'align.csv'), 'w') as csvfile:
+        with open(os.path.join(outdir, "align.csv"), "w") as csvfile:
             csvfile.write(output.to_csv())
 
         # Inline the alignment into the index.html file.
-        htmltxt = open(get_resource('www/view_alignment.html')).read()
-        htmltxt = htmltxt.replace("var INLINE_JSON;", "var INLINE_JSON=%s;" % (output.to_json()));
-        open(os.path.join(outdir, 'index.html'), 'w').write(htmltxt)
+        htmltxt = open(get_resource("www/view_alignment.html")).read()
+        htmltxt = htmltxt.replace(
+            "var INLINE_JSON;", "var INLINE_JSON=%s;" % (output.to_json())
+        )
+        open(os.path.join(outdir, "index.html"), "w").write(htmltxt)
 
-        status['status'] = 'OK'
+        status["status"] = "OK"
 
-        logging.info('done with transcription.')
+        logging.info("done with transcription.")
 
         return output
+
 
 class TranscriptionsController(Resource):
     def __init__(self, transcriber):
@@ -140,41 +151,53 @@ class TranscriptionsController(Resource):
     def render_POST(self, req):
         uid = self.transcriber.next_id()
 
-        tran = req.args.get(b'transcript', [b''])[0].decode()
-        audio = req.args[b'audio'][0]
+        tran = req.args.get(b"transcript", [b""])[0].decode()
+        audio = req.args[b"audio"][0]
 
-        disfluency = True if b'disfluency' in req.args else False
-        conservative = True if b'conservative' in req.args else False
-        kwargs = {'disfluency': disfluency,
-                  'conservative': conservative,
-                  'disfluencies': set(['uh', 'um'])}
+        disfluency = True if b"disfluency" in req.args else False
+        conservative = True if b"conservative" in req.args else False
+        kwargs = {
+            "disfluency": disfluency,
+            "conservative": conservative,
+            "disfluencies": set(["uh", "um"]),
+        }
 
         async_mode = True
-        if b'async' in req.args and req.args[b'async'][0] == b'false':
+        if b"async" in req.args and req.args[b"async"][0] == b"false":
             async_mode = False
 
         # We need to make the transcription directory here, so that
         # when we redirect the user we are sure that there's a place
         # for them to go.
-        outdir = os.path.join(self.transcriber.data_dir, 'transcriptions', uid)
+        outdir = os.path.join(self.transcriber.data_dir, "transcriptions", uid)
         os.makedirs(outdir)
 
         # Copy over the HTML
-        shutil.copy(get_resource('www/view_alignment.html'), os.path.join(outdir, 'index.html'))
+        shutil.copy(
+            get_resource("www/view_alignment.html"), os.path.join(outdir, "index.html")
+        )
 
         result_promise = threads.deferToThreadPool(
-            reactor, reactor.getThreadPool(),
+            reactor,
+            reactor.getThreadPool(),
             self.transcriber.transcribe,
-            uid, tran, audio, async_mode, **kwargs)
+            uid,
+            tran,
+            audio,
+            async_mode,
+            **kwargs
+        )
 
         if not async_mode:
+
             def write_result(result):
-                '''Write JSON to client on completion'''
+                """Write JSON to client on completion"""
                 req.setHeader("Content-Type", "application/json")
                 req.write(result.to_json(indent=2).encode())
                 req.finish()
+
             result_promise.addCallback(write_result)
-            result_promise.addErrback(lambda _: None) # ignore errors
+            result_promise.addErrback(lambda _: None)  # ignore errors
 
             req.notifyFinish().addErrback(lambda _: result_promise.cancel())
 
@@ -182,18 +205,24 @@ class TranscriptionsController(Resource):
 
         req.setResponseCode(FOUND)
         req.setHeader(b"Location", "/transcriptions/%s" % (uid))
-        return b''
+        return b""
+
 
 class LazyZipper(Insist):
     def __init__(self, cachedir, transcriber, uid):
         self.transcriber = transcriber
         self.uid = uid
-        Insist.__init__(self, os.path.join(cachedir, '%s.zip' % (uid)))
+        Insist.__init__(self, os.path.join(cachedir, "%s.zip" % (uid)))
 
     def serialize_computation(self, outpath):
-        shutil.make_archive('.'.join(outpath.split('.')[:-1]), # We need to strip the ".zip" from the end
-                            "zip",                             # ...because `shutil.make_archive` adds it back
-                            os.path.join(self.transcriber.out_dir(self.uid)))
+        shutil.make_archive(
+            ".".join(
+                outpath.split(".")[:-1]
+            ),  # We need to strip the ".zip" from the end
+            "zip",  # ...because `shutil.make_archive` adds it back
+            os.path.join(self.transcriber.out_dir(self.uid)),
+        )
+
 
 class TranscriptionZipper(Resource):
     def __init__(self, cachedir, transcriber):
@@ -202,7 +231,7 @@ class TranscriptionZipper(Resource):
         Resource.__init__(self)
 
     def getChild(self, path, req):
-        uid = path.decode().split('.')[0]
+        uid = path.decode().split(".")[0]
         t_dir = self.transcriber.out_dir(uid)
         if os.path.exists(t_dir):
             # TODO: Check that "status" is complete and only create a LazyZipper if so
@@ -216,28 +245,38 @@ class TranscriptionZipper(Resource):
         else:
             return Resource.getChild(self, path, req)
 
-def serve(port=8765, interface='0.0.0.0', installSignalHandlers=0, nthreads=4, ntranscriptionthreads=2, data_dir=get_datadir('webdata')):
+
+def serve(
+    port=8765,
+    interface="0.0.0.0",
+    installSignalHandlers=0,
+    nthreads=4,
+    ntranscriptionthreads=2,
+    data_dir=get_datadir("webdata"),
+):
     logging.info("SERVE %d, %s, %d", port, interface, installSignalHandlers)
 
     if not os.path.exists(data_dir):
         os.makedirs(data_dir)
 
-    zip_dir = os.path.join(data_dir, 'zip')
+    zip_dir = os.path.join(data_dir, "zip")
     if not os.path.exists(zip_dir):
         os.makedirs(zip_dir)
 
     f = File(data_dir)
 
-    f.putChild(b'', File(get_resource('www/index.html')))
-    f.putChild(b'status.html', File(get_resource('www/status.html')))
-    f.putChild(b'preloader.gif', File(get_resource('www/preloader.gif')))
+    f.putChild(b"", File(get_resource("www/index.html")))
+    f.putChild(b"status.html", File(get_resource("www/status.html")))
+    f.putChild(b"preloader.gif", File(get_resource("www/preloader.gif")))
 
-    trans = Transcriber(data_dir, nthreads=nthreads, ntranscriptionthreads=ntranscriptionthreads)
+    trans = Transcriber(
+        data_dir, nthreads=nthreads, ntranscriptionthreads=ntranscriptionthreads
+    )
     trans_ctrl = TranscriptionsController(trans)
-    f.putChild(b'transcriptions', trans_ctrl)
+    f.putChild(b"transcriptions", trans_ctrl)
 
     trans_zippr = TranscriptionZipper(zip_dir, trans)
-    f.putChild(b'zip', trans_zippr)
+    f.putChild(b"zip", trans_zippr)
 
     s = Site(f)
     logging.info("about to listen")
@@ -247,28 +286,46 @@ def serve(port=8765, interface='0.0.0.0', installSignalHandlers=0, nthreads=4, n
     reactor.run(installSignalHandlers=installSignalHandlers)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(
-        description='Align a transcript to audio by generating a new language model.')
-    parser.add_argument('--host', default="0.0.0.0",
-                       help='host to run http server on')
-    parser.add_argument('--port', default=8765, type=int,
-                        help='port number to run http server on')
-    parser.add_argument('--nthreads', default=multiprocessing.cpu_count(), type=int,
-                        help='number of alignment threads')
-    parser.add_argument('--ntranscriptionthreads', default=2, type=int,
-                        help='number of full-transcription threads (memory intensive)')
-    parser.add_argument('--log', default="INFO",
-                        help='the log level (DEBUG, INFO, WARNING, ERROR, or CRITICAL)')
+        description="Align a transcript to audio by generating a new language model."
+    )
+    parser.add_argument("--host", default="0.0.0.0", help="host to run http server on")
+    parser.add_argument(
+        "--port", default=8765, type=int, help="port number to run http server on"
+    )
+    parser.add_argument(
+        "--nthreads",
+        default=multiprocessing.cpu_count(),
+        type=int,
+        help="number of alignment threads",
+    )
+    parser.add_argument(
+        "--ntranscriptionthreads",
+        default=2,
+        type=int,
+        help="number of full-transcription threads (memory intensive)",
+    )
+    parser.add_argument(
+        "--log",
+        default="INFO",
+        help="the log level (DEBUG, INFO, WARNING, ERROR, or CRITICAL)",
+    )
 
     args = parser.parse_args()
 
     log_level = args.log.upper()
     logging.getLogger().setLevel(log_level)
 
-    logging.info('gentle %s' % (gentle.__version__))
-    logging.info('listening at %s:%d\n' % (args.host, args.port))
+    logging.info("gentle %s" % (gentle.__version__))
+    logging.info("listening at %s:%d\n" % (args.host, args.port))
 
-    serve(args.port, args.host, nthreads=args.nthreads, ntranscriptionthreads=args.ntranscriptionthreads, installSignalHandlers=1)
+    serve(
+        args.port,
+        args.host,
+        nthreads=args.nthreads,
+        ntranscriptionthreads=args.ntranscriptionthreads,
+        installSignalHandlers=1,
+    )
