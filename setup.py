321626d8859b70c86875755a8a2c4a3875414e46
jamesm62 <james@kapwing.com>
2024-07-30 10:46:17 -0700

    Revert "install zope.interface instead of just zope"
    This reverts commit 6ad23eaeabe4e9fd0c30775898a52436fef2766f.

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setup(
     author="Robert M Ochshorn",
     license="MIT",
     packages=["gentle"],
-    install_requires=["zope.interface", "twisted"],
+    install_requires=["zope", "twisted"],
     test_suite="tests",
 )
